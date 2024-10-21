abd048253c8bc0c040ad5738d5b3581a9dba74ac
johtok <johannestoke@gmail.com>
2024-10-22 00:21:13 +0200

    vscode support

--- wsl.nix
+++ wsl.nix
@@ -20,7 +20,7 @@
   security.sudo.wheelNeedsPassword = false;
 
   # FIXME: uncomment the next line to enable SSH
-  # services.openssh.enable = true;
+  services.openssh.enable = true;
 
   users.users.${username} = {
     isNormalUser = true;
@@ -68,20 +68,20 @@
   # FIXME: uncomment the next block to make vscode running in Windows "just work" with NixOS on WSL
   # solution adapted from: https://github.com/K900/vscode-remote-workaround
   # more information: https://github.com/nix-community/NixOS-WSL/issues/238 and https://github.com/nix-community/NixOS-WSL/issues/294
-  # systemd.user = {
-  #   paths.vscode-remote-workaround = {
-  #     wantedBy = ["default.target"];
-  #     pathConfig.PathChanged = "%h/.vscode-server/bin";
-  #   };
-  #   services.vscode-remote-workaround.script = ''
-  #     for i in ~/.vscode-server/bin/*; do
-  #       if [ -e $i/node ]; then
-  #         echo "Fixing vscode-server in $i..."
-  #         ln -sf ${pkgs.nodejs_18}/bin/node $i/node
-  #       fi
-  #     done
-  #   '';
-  # };
+   systemd.user = {
+     paths.vscode-remote-workaround = {
+       wantedBy = ["default.target"];
+       pathConfig.PathChanged = "%h/.vscode-server/bin";
+     };
+     services.vscode-remote-workaround.script = ''
+       for i in ~/.vscode-server/bin/*; do
+         if [ -e $i/node ]; then
+           echo "Fixing vscode-server in $i..."
+           ln -sf ${pkgs.nodejs_18}/bin/node $i/node
+         fi
+       done
+     '';
+   };
 
   nix = {
     settings = {
