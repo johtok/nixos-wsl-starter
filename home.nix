514197bc4e699190ac9ced83cbddadae47d40d83
johtok <johannestoke@gmail.com>
2024-10-22 00:28:16 +0200

    adding secrets

--- home.nix
+++ home.nix
@@ -1,6 +1,6 @@
 {
   # FIXME: uncomment the next line if you want to reference your GitHub/GitLab access tokens and other secrets
-  # secrets,
+  secrets,
   pkgs,
   username,
   nix-index-database,
@@ -162,14 +162,14 @@ in {
       userName = "johtok"; #FIXME: set your git username
       extraConfig = {
         # FIXME: uncomment the next lines if you want to be able to clone private https repos
-        # url = {
-        #   "https://oauth2:${secrets.github_token}@github.com" = {
-        #     insteadOf = "https://github.com";
-        #   };
+         url = {
+           "https://oauth2:${secrets.github_token}@github.com" = {
+             insteadOf = "https://github.com";
+           };
         #   "https://oauth2:${secrets.gitlab_token}@gitlab.com" = {
         #     insteadOf = "https://gitlab.com";
         #   };
-        # };
+         };
         push = {
           default = "current";
           autoSetupRemote = true;
--- wsl.nix
+++ wsl.nix
@@ -1,6 +1,6 @@
 {
   # FIXME: uncomment the next line if you want to reference your GitHub/GitLab access tokens and other secrets
-  # secrets,
+  secrets,
   username,
   hostname,
   pkgs,
@@ -87,10 +87,10 @@
     settings = {
       trusted-users = [username];
       # FIXME: use your access tokens from secrets.json here to be able to clone private repos on GitHub and GitLab
-      # access-tokens = [
-      #   "github.com=${secrets.github_token}"
+       access-tokens = [
+         "github.com=${secrets.github_token}"
       #   "gitlab.com=OAuth2:${secrets.gitlab_token}"
-      # ];
+       ];
 
       accept-flake-config = true;
       auto-optimise-store = true;
